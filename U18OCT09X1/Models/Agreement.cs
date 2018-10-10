b7ef0f579cc85ad370a68cc7c6d2152dd144f2b6
Drohano <joix99@live.se>
2018-10-10 16:53:25 +0200

    added in rentalcontroler + agreemenr models

--- U18OCT09X1/Models/Agreement.cs
+++ U18OCT09X1/Models/Agreement.cs
@@ -7,6 +7,14 @@ namespace U18OCT09X1.Models
 {
     public class Agreement
     {
-        public string id { get; set; }
+        public string Id { get; set; }
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+
+        public Car Car { get; set; }
+
+        public UserRecord UserRecord { get; set; }
     }
 }
