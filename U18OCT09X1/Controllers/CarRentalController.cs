5e8df2e64a7b495fb7726c2fe47d5e72c8b9c5af
Drohano <joix99@live.se>
2018-10-11 11:59:11 +0200

    changed name of rentalcontroller added veiws

---
+++ U18OCT09X1/Controllers/CarRentalController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using U18OCT09X1.Models;
+
+namespace U18OCT09X1.Controllers
+{
+    public class CarRentalController : Controller
+    {
+        //fejkad data (ska kolla upp)
+        private List<Car> cars;
+        private List<UserRecord> customers;
+
+        public CarRentalController()
+        {
+            cars = new List<Car>();
+            cars.Add(new Models.Car() { Registration = "abc123", Brand = "Volvo", Model = "Big boi" });
+            cars.Add(new Models.Car() { Registration = "abc123", Brand = "Volvo", Model = "Big boi" });
+            cars.Add(new Models.Car() { Registration = "abc123", Brand = "Volvo", Model = "Big boi" });
+            cars.Add(new Models.Car() { Registration = "abc123", Brand = "Volvo", Model = "Big boi" });
+
+            customers = new List<UserRecord>();
+            customers.Add(new UserRecord() {FirstName="DERP"});
+        }
+
+        // GET: Rental
+        public ActionResult CarsView()
+        {
+            return View(cars);
+        }
+
+        public ViewResult Details(string Regestration)
+        {
+            return View("CarDetails", cars.First(x => x.Registration == Regestration));
+        }
+     
+        public PartialViewResult Cars()
+        {
+            return PartialView("_Cars", cars);
+
+        }
+        public PartialViewResult Customers()
+        {
+
+            return PartialView("_Customers", customers);
+        }
+
+    }
+}
